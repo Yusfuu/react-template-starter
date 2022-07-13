b75b85f2a467fcddfcd795b554cdff08e5e8cf75
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-07-13 22:39:58 +0100

    update vite v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Product, IProduct } from '@/components/Product';
+import { IProduct, Product } from '@/components/Product';
 import { useState } from 'react';
 
 const App = () => {
@@ -27,7 +27,6 @@ const App = () => {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
-
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <Product key={product.id} product={product} />
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { store } from './app/store';
 import './i18n';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
