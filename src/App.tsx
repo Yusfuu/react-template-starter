809521d0f8372433cc14e75a4565ee19bd893b79
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-07-13 23:04:25 +0100

    add some template

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { IProduct, Product } from '@/components/Product';
-import { useState } from 'react';
+import { Header } from '@/components/Header';
+import { Product } from '@/components/Product';
+import { useProducts } from './hooks/useProducts';
 
 const App = () => {
-  const [products, setProducts] = useState<IProduct[]>([
-    {
-      id: 1,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: '$35',
-      color: 'Black',
-    },
-    {
-      id: 2,
-      name: 'Basic Tee 6-Pack',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: '$35',
-      color: 'Black',
-    },
-  ]);
+  const { products } = useProducts();
 
   return (
     <div className="bg-white">
+      <Header />
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
