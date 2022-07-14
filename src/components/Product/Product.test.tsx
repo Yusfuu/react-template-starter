236f9844095a7912ffbd9389821b48e505336ed2
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-07-14 03:39:27 +0100

    adding testing library vitest

--- src/components/Product/Product.test.tsx
+++ src/components/Product/Product.test.tsx
@@ -1,5 +1,20 @@
-import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { IProduct, Product } from '.';
 
-test('one plus one is two', () => {
-  expect(1 + 1).toBe(2);
+describe('Simple working test', () => {
+  it('the title of product is visible', () => {
+    const product: IProduct = {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    };
+
+    render(<Product product={product} />);
+    expect(screen.getByText(product.name)).toBeInTheDocument();
+  });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  test: {},
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests.ts',
+    globals: true,
+  },
 });
