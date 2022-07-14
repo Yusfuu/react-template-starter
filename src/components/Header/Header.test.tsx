32d41a1d8ff31aa85171d5756319f6c4297b1ffa
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-07-14 02:04:35 +0100

    add testd

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,1 +1,5 @@
-export {};
+import { expect, test } from 'vitest';
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
--- src/components/Product/Product.test.tsx
+++ src/components/Product/Product.test.tsx
@@ -1,1 +1,5 @@
-export {};
+import { expect, test } from 'vitest';
+
+test('one plus one is two', () => {
+  expect(1 + 1).toBe(2);
+});
